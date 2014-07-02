7a8b165509199c8c53235c55fc5c85e05640afda
Brian Iversen <biversen21@gmail.com>
2014-07-02 15:30:27 -0700

    Updated views and started validations

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,11 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :accounts
+  
+  resources :employees do
+    resources :time_entries
+  end
 
   # Example resource route with options:
   #   resources :products do
